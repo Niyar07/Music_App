6d919e7b98a92d72c9a28af80e14579f102ffb33
Niyar Rane <raneniyar2004@gmail.com>
2025-08-02 15:39:43 +0530

    Create Sign-Up Page

--- client/lib/features/auth/view/pages/signup_page.dart
+++ client/lib/features/auth/view/pages/signup_page.dart
@@ -1,6 +1,7 @@
 import 'package:client/core/theme/app_pallete.dart';
 import 'package:client/features/auth/view/widgets/auth_gradient_button.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import '../widgets/custom_field.dart';
 
@@ -12,58 +13,79 @@ class SignupPage extends StatefulWidget {
 }
 
 class _SignupPageState extends State<SignupPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+    formKey.currentState!.validate();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              "Sign Up.",
-              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(
-              height: 40,
-            ),
-            CustomField(
-              hintText: 'Name',
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            CustomField(
-              hintText: 'Email',
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            CustomField(
-              hintText: 'Password',
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            AuthGradientButton(),
-            const SizedBox(
-              height: 20,
-            ),
-            RichText(
-              text: TextSpan(
-                text: 'Already have an Account? ',
-                style: Theme.of(context).textTheme.titleMedium,
-                children: [
-                  TextSpan(
-                      text: 'Sign in',
-                      style: TextStyle(
-                          color: Pallete.gradient2,
-                          fontWeight: FontWeight.bold)),
-                ],
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Sign Up.",
+                style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 40,
+              ),
+              CustomField(
+                controller: nameController,
+                hintText: 'Name',
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              CustomField(
+                controller: emailController,
+                hintText: 'Email',
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              CustomField(
+                controller: passwordController,
+                hintText: 'Password',
+                isObscureText: true,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              AuthGradientButton(),
+              const SizedBox(
+                height: 20,
+              ),
+              RichText(
+                text: TextSpan(
+                  text: 'Already have an Account? ',
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                        text: 'Sign in',
+                        style: TextStyle(
+                            color: Pallete.gradient2,
+                            fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
